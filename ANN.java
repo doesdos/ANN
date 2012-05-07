317444f17a8a08e4eb1582209b53d1fbd7a2f86b
Matthew Clemens <mclemens66+github@gmail.com>
2012-05-06 17:03:56 -0700

    using bias input and bias hidden node

--- ANN.java
+++ ANN.java
@@ -20,31 +20,34 @@ public class ANN {
 
     /* deltas to be calculated using  gradient descent on the weight functions */
     public double[][] deltaOut, deltaHidden;
+
     /* deltas cached for use with momentum adjustments */
-    public double[][] deltaOutMem, deltaHiddenMem;
+    //public double[][] deltaOutMem, deltaHiddenMem;
 
     /* The learning rate should be something around .1
      * the momentum is again around the .1 mark,
      * and errorConverganceThreshold is the error you will except if you 
      * want to terminate early */
-    public ANN(int inputDimension, int numHidden, int numOutputClasses,
+    public ANN(int input_dimension, int num_hidden, int numOutputClasses, int epochMax, double learningRate){
                 // double momentum, double errorConverganceThreshold
-               int epochMax, double learningRate){
 
-        this.inputDimension = inputDimension;
+        // account for bias input at x_0
+        this.inputDimension = input_dimension+1;
 
-        this.numHidden = numHidden;
+        this.numHidden = num_hidden;
         // row major
         this.hiddenWeights = new double[numHidden][inputDimension];
         this.deltaHidden = new double[numHidden][inputDimension];
-        this.deltaHiddenMem = new double[numHidden][inputDimension];
-        double [] hiddenOut =  new double[numHidden];
+        //this.deltaHiddenMem = new double[numHidden][inputDimension];
+        this.hiddenOut =  new double[numHidden];
+        //bias input is always 1
+        this.hiddenOut[0]=1;
 
         this.numOutputClasses = numOutputClasses;
         // row major
         this.outputWeights = new double[numOutputClasses][numHidden];
         this.deltaOut = new double[numOutputClasses][numHidden];
-        this.deltaOutMem = new double[numOutputClasses][numHidden];
+        //this.deltaOutMem = new double[numOutputClasses][numHidden];
         this.output = new double[numOutputClasses];
 
         /* options*/
@@ -61,14 +64,16 @@ public class ANN {
      * incremental non zero values count the number of iterations run */
     public double [] offlineLearning(double [][] input, double [][] expected){
         double [] error = new double[epochMax]; 
+        
         // Fisher yates inplace shuffle of input and expected
-        int m = input[0].length;
+        int m = input.length;
+        System.out.println("The Number of vectors is "+ m);
         int i;
         double[] temp1, temp2;
+       
         while(m>0){
             //pick random remaining element
             i = (int)Math.floor((Math.random() *m-- ));
-        System.out.println("THE ARRAY INDEX IS "+m);
             temp1 = input[m];
             temp2 = expected[m];
 
@@ -81,27 +86,29 @@ public class ANN {
 
         // start the training 
         // TODO terminate early
-        i = 0;
-        m = input[0].length;
-        while(i<epochMax){
+        int time = 0 ;
+        double singleError;
+        while(time<epochMax){
             double tempError = 0;
-            while(m>=0){
-                this.feedForward(input[m]);
+            for(int x=0; x<input.length; x++){
+               double [] e =  this.feedForward(input[x]);
                 // check the error of the single input
-                tempError += this.calcError(this.output, expected[m]);
-                this.compute(input[m], expected[m]);
+                singleError = this.calcError(e, expected[x]);
+                System.out.println("Single Error is "+ singleError);
+                tempError += singleError;
+            
+                this.compute(input[x], expected[x]);
                 this.adjust();
-                m--;
-            }
-            m = input[0].length;
-            error[i] = tempError;
+            } 
+            error[time] = tempError;
+            time++;
         }
             return error;
     }
 
-    public void feedForward(double [] input ){
+    public double [] feedForward(double [] input ){
         this.calcHiddenFromInput(input);
-        this.calcOutputFromHidden();
+        return this.calcOutputFromHidden();
     }
 
     public void compute(double [] input, double [] expected ){
@@ -117,39 +124,52 @@ public class ANN {
     public void setWeights(){
         /*  Random double (0,1) rather than [0,1), 
         * and really close to 1 source: http://bit.ly/JoWZRw */
-        for(int h=1; h<this.numHidden; h++){
+        for(int h=0; h<this.numHidden; h++){
             for(int j=0; j<this.inputDimension; j++){
                 // [hidden][input]
-                hiddenWeights[h][j] = (0.01 + (Math.random() * (1 - 0.01)));
+                //hiddenWeights[h][j] = (0.01 + (Math.random() * (1 - 0.01)));
+                hiddenWeights[h][j] =  (Math.random() * 2)-1;
             }
-            for(int x=1; h<this.numOutputClasses; h++){
+            for(int x=0; x<this.numOutputClasses; x++){
                 // [output][hidden]
-                outputWeights[x][h] = (0.01 + (Math.random() * (1 - 0.01)));
+                //outputWeights[x][h] = (0.01 + (Math.random() * (1 - 0.01)));
+                outputWeights[x][h] = (Math.random() * 2)-1;
             }
         }
     }
 
-    public void  calcHiddenFromInput(double [] input){
-        /* this is some sort of default weight as suggested in paper */
-        hiddenWeights[0][0] = 1;
-        double sumWeight = 0;
-
-        for(int h=1; h<this.numHidden; h++){
+    public void setWeightsToOne(){
+        for(int h=0; h<this.numHidden; h++){
             for(int j=0; j<this.inputDimension; j++){
+                // [hidden][input]
+                hiddenWeights[h][j] = 1;
+            }
+            for(int x=0; x<this.numOutputClasses; x++){
+                // [output][hidden]
+                outputWeights[x][h] = 1;
+            }
+        }
+    }
+
+    public void calcHiddenFromInput(double [] input){
+        double sumWeight = 0;
+        for(int h=1; h<this.numHidden; h++){
+            for(int j=0; j<this.inputDimension-1; j++){
                 sumWeight += input[j] * hiddenWeights[h][j];
             }
-
+            /* this is some sort of default weight as suggested in paper */
+            sumWeight += 1 * hiddenWeights[h][inputDimension-1];
             this.hiddenOut[h] = this.sigmoid(sumWeight);
         }
     }
 
-    public void calcOutputFromHidden(){
+    public double [] calcOutputFromHidden(){
         // used for soft max
         double total = 0;
         double [] preSoftmaxOutput = new double[numOutputClasses];
         double sumWeight = 0;
         /* default weight again */
-        outputWeights[0][0] = 1;
+        this.hiddenOut[0] = 1;
 
         for(int i=1; i< this.numOutputClasses; i++){
             for(int h=0; h< this.numHidden; h++){
@@ -163,49 +183,54 @@ public class ANN {
             /* softmax to output */
             this.output[i] = Math.exp(preSoftmaxOutput[i]) / total;
          }
+        return output;
     }
 
-     public void calcDeltaOutput(double [] expected){
-         for(int i = 1;i<this.numOutputClasses; i++){
-             for(int h =0;h< this.numHidden; h++){
-                 deltaOut[i][h] =  this.learningRate *(expected[i] - output[i])* hiddenOut[h];
+    public void calcDeltaOutput(double [] expected){
+        for(int i = 1;i<this.numOutputClasses; i++){
+            for(int h =0;h< this.numHidden; h++){
+                 deltaOut[i][h] = this.learningRate *(expected[i] - output[i])* hiddenOut[h];
              }
          }
      }
 
-     public void calcDeltaHidden(double [] input, double [] expected){
+
+    public void calcDeltaHidden(double [] input, double [] expected){
          // damn this has a cubic runtime not fast at all.
          // speedups, use previous calcDeltaOutput or something??
          for(int h=1;h<this.numHidden; h++){
-             for(int j=0; j<this.inputDimension;j++){
+             for(int j=0; j<this.inputDimension-1; j++){
                  double temp = 0;
                  for(int i=1; i<this.numOutputClasses; i++){
                      temp += (expected[i] - output[i]) * outputWeights[i][h];
                  }
+                 temp += (expected[i] - 1) * outputWeights[i][h];
                  deltaHidden[h][j] = this.learningRate * temp * hiddenOut[h]* (1-hiddenOut[h]) * input[j];
              }
          }
      }
 
-     public void updateOutputWeight(){
+    public void updateOutputWeight(){
          for(int i=1; i<this.numOutputClasses; i++){
              for(int h=0; h<this.numHidden; h++){
-                 outputWeights[i][h] =
-                     outputWeights[i][h] + deltaOut[i][h];//+ (this.momentum*deltaOut[i][h]);
+                 outputWeights[i][h] = outputWeights[i][h] + deltaOut[i][h];
+                 //+ (this.momentum*deltaOut[i][h]);
                  //TODO momentum
              }
          }
      }
-     public void updateHiddenWeight(){
+
+    public void updateHiddenWeight(){
          for(int h=1; h<this.numHidden; h++){
-             for(int j=0; j<this.inputDimension; j++){
-                 hiddenWeights[h][j] = 
-                     hiddenWeights[h][j] + deltaHidden[h][j];// +(deltaHidden[h][j]* this.momentum); 
+             for(int j=0; j<this.inputDimension-1; j++){
+                 hiddenWeights[h][j] = hiddenWeights[h][j] + deltaHidden[h][j];
+                 // +(deltaHidden[h][j]* this.momentum); 
                  //TODO momentum
              }
          }
      }
-    /*  node threshold function that detrimens the nodes output */
+
+    /*   threshold function for hidden output */
     public double sigmoid(double x) {
         return (1 / (1 + Math.exp(-x)));
     }
@@ -227,6 +252,12 @@ public class ANN {
         return outputs;
      }
 
+    public double calcError(double result, double target) {
+        double diff;
+        diff = result-target;
+        return diff*diff*.5;
+    }
+    
      /*Sum of squares error for one epoch of data */
     public double calcError(double[] result, double[] target) {
         double error = 0.0;
--- ANNTest.java
+++ ANNTest.java
@@ -3,20 +3,108 @@ import org.junit.Test;
 
 public class ANNTest {
 
+    @Test
+    public void test_setWeights(){
+       ANN net = new ANN(2,2,1,100,.6);
+       for(int x=0; x<net.numHidden;x++){
+           for(int y=0; y<net.inputDimension;y++){
+                //System.out.println(
+                //"Weight for ("+x+","+y+"): +" + net.hiddenWeights[x][y]);
+                assertFalse(net.hiddenWeights[x][y] ==0);
+           }
+           for(int z=0;z<net.numOutputClasses; z++){
+                //System.out.println(
+                //"Weight for ("+x+","+y+"): +" + net.outputWeights[z][x]);
+               assertFalse(net.outputWeights[z][x]  == 0);
+           }
+       }
+       net.setWeightsToOne();
+        for(int x=0; x<net.numHidden;x++){
+           for(int y=0; y<net.inputDimension;y++){
+                //System.out.println(
+                //"Weight for ("+x+","+y+"): +" + net.hiddenWeights[x][y]);
+                assertEquals(1,net.hiddenWeights[x][y],.00001);
+           }
+           for(int z=0;z<net.numOutputClasses; z++){
+                //System.out.println(
+                //"Weight for ("+x+","+y+"): +" + net.outputWeights[z][x]);
+               assertEquals(1, net.outputWeights[z][x],.00001);
+           }
+       }
+    }
 
     @Test
-    public void test_ANN_construct(){
-        double xorInput[][] ={ 
-           {0.0,0.0},
+    public void test_inputToHidden(){
+        double  input [] = {2.0,2.0,2.0};
+        ANN net = new ANN(3,2,3,10,.5);
+        net.setWeightsToOne();
+        net.calcHiddenFromInput(input);
+        assertEquals(1, net.hiddenOut[0],0);
+        for(int x=1;x<net.hiddenOut.length; x++){
+            // it's 7 rather than 6 because of that annoying bias weight of 1 
+            assertEquals(net.sigmoid(7),net.hiddenOut[x],0.0001);
+        }
+    }
+  
+    @Test
+    public void test_hiddentToOutput(){
+        double sum =0;
+        double  input [] = {2.0,2.0,2.0};
+        ANN net = new ANN(3,3,3,10,.5);
+        net.setWeightsToOne();
+        net.calcHiddenFromInput(input);
+        net.calcOutputFromHidden();
+        for(int x=1;x<net.numOutputClasses; x++){
+            sum+= net.output[x];
+        }
+            assertEquals(1 ,sum, .0001);
+    }
+
+    @Test
+    public void test_feedForward(){
+        double xorIn[][] ={ 
+           {0.4,0.0},
+           {1.0,0.3},
+           {0.0,1.4},
+           {1.0,1.0}};
+
+        double  input [] = {2.0,2.0,2.0};
+        ANN net = new ANN(2,3,3,10,.5);
+        double [] e = net.feedForward(xorIn[0]);
+        assertArrayEquals(e, net.output, .001);
+        e = net.feedForward(xorIn[3]);
+        assertArrayEquals(e, net.output, .001);
+    }
+
+    @Test
+    public void test_calcDeltas(){
+
+    }
+
+    @Test
+    public void test_compute(){
+    }
+
+    @Test
+    public void ANN_construct(){
+        double xorIn[][] ={ 
+           {1.0,0.0},
            {1.0,0.0},
            {0.0,1.0},
            {1.0,1.0}};
 
-        double xorIdeal[][] =
-          { {0.0},{1.0},{1.0},{0.0}};
+        double xorOut[][] = { {0.0},{1.0},{1.0},{0.0}};
 
-       ANN net = new ANN(2,3,1,50,.6);
-        double [] errors = net.offlineLearning(xorInput, xorIdeal);
+        double [] t = {1.0, 0.0};
+
+       ANN net2 = new ANN(2,3,1,10,.1);
+        double [] errors = net2.offlineLearning(xorIn, xorOut);
+        for(int i=0; i<errors.length;i++){
+            System.out.println("errors  "+errors[i]);
         }
-
+         double [] ans =  net2.feedForward(t);
+        for(int i=0; i<ans.length;i++){
+            System.out.println("THE  answer is  "+ans[i]);
+        }
+    }
 }
